335487ef321ad7d2a89cc42df985debcad5bc63f
WormBit <19507383+WormBit@users.noreply.github.com>
2024-02-06 19:38:21 -0600

    Create html and js script file with pseudocode

---
+++ rps.js
@@ -0,0 +1,13 @@
+// Rock Paper Scissors by Atlas
+
+/* STEPS FOR RPS
+1. CREATE a list of potential "hands" for computer to choose from e.g. scissors, paper, rock.
+2. have computer CHOOSE one of the options at RANDOM
+3. have user CHOOSE an option with INPUT
+4. CHECK IF user's option beats, ties, or loses against CPU's option.
+5. OUTPUT to console each player's hand, which beats which, and wether the user won, tied, or lost.
+6. LOOP through steps 1-5, 5 times.
+*/
+
+
+
